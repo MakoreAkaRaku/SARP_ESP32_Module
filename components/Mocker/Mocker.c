9dd36bd6ba30041dcc8c389c112e8be646c61dae
MakoreAkaRaku <marcromancolom@gmail.com>
2025-03-15 10:45:18 +0100

    hotfix for the residual operations

--- components/Mocker/Mocker.c
+++ components/Mocker/Mocker.c
@@ -1,14 +1,16 @@
 #include <stdlib.h>
 #include "Mocker.h"
 
-float GetTemperature() {
-    float matress = (rand()%4000)/4000.0f;
-    float value = rand()/40.f;
-    return value+matress;
+float GetTemperature()
+{
+    float matress = (rand() % 4000) / 4000.0f;
+    int value = rand() % 40;
+    return value + matress;
 }
 
-float GetHumidity() {
-    float matress = (rand()%4000)/4000.0f;
-    float value = rand()/100.f;
-    return value+matress;
+float GetHumidity()
+{
+    float matress = (rand() % 4000) / 4000.0f;
+    int value = rand() % 100;
+    return value + matress;
 }
