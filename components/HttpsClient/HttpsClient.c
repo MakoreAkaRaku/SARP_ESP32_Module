5cc878369b5e97675d3d7b5602152cac62f1b5dc
MakoreAkaRaku <marcromancolom@gmail.com>
2025-06-22 14:46:07 +0200

    Implemented RegisterModule functionality; only one-time use for registering the module into the server for then always calling it up

--- components/HttpsClient/HttpsClient.c
+++ components/HttpsClient/HttpsClient.c
@@ -1,14 +1,13 @@
 #include "HttpsClient.h"
 #include "esp_log.h"
+#include "esp_crt_bundle.h"
+#include "cJson.h"
+#define MODULE_REGISTRY_SERVER_RESPONSE_SIZE 128 // Size of the response buffer for module registration
 static const char TAG[] = "HTTPSClient";
 /* Root cert for the web, taken from firefox navigator by asking for the certificate
    To embed it in the app binary, the PEM file is named
    in the component.mk COMPONENT_EMBED_TXTFILES variable.
 */
-
-#define SERVER_URL_API "https://sarp01.westeurope.cloudapp.azure.com/api"
-extern const char server_root_cert_pem_start[] asm("_binary_server_root_pem_start");
-extern const char server_root_cert_pem_end[] asm("_binary_server_root_pem_end");
 
 static esp_err_t _http_event_handler(esp_http_client_event_t *evt)
 {
@@ -30,9 +29,8 @@ static esp_err_t _http_event_handler(esp_http_client_event_t *evt)
     ESP_LOGD(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
     if (!esp_http_client_is_chunked_response(evt->client))
     {
+      memcpy(evt->user_data, evt->data, evt->data_len); // Copy data to user_data buffer
       // Write data received to stdout
-      // If you're building a real application, you'd store this in a buffer
-      // or process it directly.
       printf("%.*s", evt->data_len, (char *)evt->data);
     }
     break;
@@ -51,27 +49,30 @@ static esp_err_t _http_event_handler(esp_http_client_event_t *evt)
 
 /**
  * @brief Performs an HTTP request with a given method and URL.
+ * Headers are set as json content type if the method is POST/PUT/PATCH.
  *
  * @param method The HTTP method (e.g., HTTP_METHOD_GET, HTTP_METHOD_POST).
  * @param url The URL for the request.
  * @param post_data Optional: Data to send in the request body for POST/PUT.
  * @param response_buffer Buffer to store the response data; must be large enough to hold the response.
- * @param buffer_len Length of the response buffer.
+ * @param resp_buff_leng Length of the response buffer.
  * @return esp_err_t ESP_OK on success, otherwise an error code.
  */
-esp_err_t perform_http_request(esp_http_client_method_t method,
+esp_err_t PerformHttpRequest(esp_http_client_method_t method,
                                const char *url,
                                const char *post_data,
                                char *response_buffer,
-                               size_t buffer_len)
+                               size_t resp_buff_leng)
 {
   // Init config struct
   esp_http_client_config_t config = {
-      .cert_pem = server_root_cert_pem_start,
+      .crt_bundle_attach = esp_crt_bundle_attach, // Use the built-in certificate bundle
       .url = url,
       .method = method,
       .event_handler = _http_event_handler, // Always good to have an event handler
-      .timeout_ms = 10000,                  // Set a timeout for the request
+      .timeout_ms = 100000,                 // Set a timeout for the request
+      .user_data = response_buffer,         // Pass the response buffer to the event handler
+      .buffer_size = resp_buff_leng,        // Set the buffer size for the response
   };
 
   // Init HTTP client
@@ -82,7 +83,7 @@ esp_err_t perform_http_request(esp_http_client_method_t method,
     return ESP_FAIL;
   }
 
-  // 3. If request method is sends data, use of update POST data if provided
+  //If request method is sends data, use of update POST data if provided
   if (method == HTTP_METHOD_POST || method == HTTP_METHOD_PUT || method == HTTP_METHOD_PATCH)
   {
     if (post_data != NULL)
@@ -94,7 +95,7 @@ esp_err_t perform_http_request(esp_http_client_method_t method,
         esp_http_client_cleanup(client);
         return err;
       }
-      esp_http_client_set_header(client, "Content-Type", "application/json"); // Example for JSON
+      esp_http_client_set_header(client, "Content-Type", "application/json");
     }
     else
     {
@@ -113,29 +114,6 @@ esp_err_t perform_http_request(esp_http_client_method_t method,
              (method == HTTP_METHOD_GET) ? "GET" : ((method == HTTP_METHOD_POST) ? "POST" : "OTHER"),
              esp_http_client_get_status_code(client),
              content_length);
-    long long int total_read = 0;
-    if (content_length > 0)
-    { // Only read if there's content to read
-      int read_len = 0;
-      while (total_read < buffer_len - 1)
-      { // Leave space for null terminator
-        read_len = esp_http_client_read(client, response_buffer + total_read, buffer_len - 1 - total_read);
-        if (read_len == 0)
-        { // No more data
-          ESP_LOGD(TAG, "No more data to read.");
-          break;
-        }
-        else if (read_len < 0)
-        { // Error
-          ESP_LOGE(TAG, "esp_http_client_read failed:");
-          err = ESP_FAIL; // Indicate an error occurred during read
-          break;
-        }
-        total_read += read_len;
-        ESP_LOGD(TAG, "Read %d bytes, total %lld", read_len, total_read);
-      }
-    }
-    response_buffer[total_read] = '\0'; // Null-terminate the response buffer
   }
   else
   {
@@ -144,6 +122,93 @@ esp_err_t perform_http_request(esp_http_client_method_t method,
 
   // Clean up client
   esp_http_client_cleanup(client);
-
   return err;
 }
+
+/**
+ * @brief Returns the module string with the module registration response.
+ * 
+ * @param token_api 
+ * @return char* 
+ */
+const char *RegisterModule(const char *token_api)
+{
+  // Prepare the URL and request body
+  char *url = malloc(strlen(SERVER_URL_API) + strlen(MODULE_URL) +1 ); // Malloc of 128 bytes for URL plus 1 for null terminator
+  if (url == NULL)
+  {
+    ESP_LOGE(TAG, "Memory allocation failed for URL");
+    return NULL;
+  }
+  strcpy(url, SERVER_URL_API);
+  strcat(url, MODULE_URL);
+
+  cJSON *json_token_api = cJSON_CreateObject();
+  if (json_token_api == NULL)
+  {
+    ESP_LOGE(TAG, "Failed to create JSON object");
+    free(url);
+    return NULL;
+  }
+
+  cJSON_AddStringToObject(json_token_api, "token_api", token_api);
+  char *post_data = cJSON_PrintUnformatted(json_token_api);
+  cJSON_Delete(json_token_api);
+
+  if (post_data == NULL)
+  {
+    ESP_LOGE(TAG, "Failed to create JSON string");
+    free(url);
+    return NULL;
+  }
+
+  // Prepare response buffer
+  char *server_response = malloc(MODULE_REGISTRY_SERVER_RESPONSE_SIZE*sizeof(char)); // Malloc of 128 bytes for response buffer
+  if (server_response == NULL)
+  {
+    ESP_LOGE(TAG, "Memory allocation failed for response buffer");
+    free(url);
+    free(post_data);
+    return NULL;
+  }
+
+  // Perform the HTTP request
+  esp_err_t err = PerformHttpRequest(HTTP_METHOD_POST, url, post_data, server_response, MODULE_REGISTRY_SERVER_RESPONSE_SIZE);
+  
+  // Free allocated resources
+  free(url);
+  free(post_data);
+
+  if (err != ESP_OK)
+  {
+    free(server_response);
+    return NULL;
+  }
+
+  cJSON *json_response = cJSON_Parse(server_response);
+  if(json_response->type != cJSON_Object)
+  {
+    ESP_LOGE(TAG, "Response is not a valid JSON object");
+    cJSON_Delete(json_response);
+    free(server_response);
+    return NULL;
+  }
+  cJSON *tmp_pointer = cJSON_GetObjectItem(json_response, "moduleToken");
+  if (tmp_pointer == NULL || tmp_pointer->type != cJSON_String)
+  {
+    ESP_LOGE(TAG, "Response does not contain 'moduleToken' or it is not a string");
+    cJSON_Delete(json_response);
+    free(server_response);
+    return NULL;
+  }
+  const char *response = strdup(tmp_pointer->valuestring); // Duplicate the string to return
+  if(response == NULL)
+  {
+    ESP_LOGE(TAG, "Memory allocation failed for response string");
+    cJSON_Delete(json_response);
+    free(server_response);
+    return NULL;
+  }
+  return response;
+}
+
--- components/HttpsClient/HttpsClient.h
+++ components/HttpsClient/HttpsClient.h
@@ -1,10 +1,12 @@
-
 #include "esp_http_client.h"
 
+#define SERVER_URL_API "https://sarp01.westeurope.cloudapp.azure.com/api"
+#define MODULE_URL "/module/"
 
 static esp_err_t _http_event_handler(esp_http_client_event_t *evt);
-esp_err_t perform_http_request(esp_http_client_method_t method,
+esp_err_t PerformHttpRequest(esp_http_client_method_t method,
                                const char *url,
                                const char *post_data,
                                char *response_buffer,
                                size_t buffer_len);
+const char *RegisterModule(const char *token_api);
